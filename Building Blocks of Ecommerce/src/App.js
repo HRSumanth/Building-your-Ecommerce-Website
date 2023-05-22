c4b349a0082c3a4dee56e3a128c6056824d15531
HRSumanth <130749610+HRSumanth@users.noreply.github.com>
2023-05-22 19:08:04 +0530

    Add files via upload

---
+++ Building Blocks of Ecommerce/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/products/Header';
+import Products from './components/products/productlist';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+     <Products></Products>
+    </div>
+  );
+}
+
+export default App;
---
+++ Building Blocks of Ecommerce/src/components/products/productlist.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const Products = () => {
+  const productsArr = [
+    {
+      title: 'Colors',
+      price: 100,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
+    },
+    {
+      title: 'Black and white Colors',
+      price: 50,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
+    },
+    {
+      title: 'Yellow and Black Colors',
+      price: 70,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
+    },
+    {
+      title: 'Blue Color',
+      price: 100,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
+    },
+  ];
+
+  return (
+    <div className="container">
+      <h1>Products</h1>
+      <div className="row">
+        {productsArr.map((product, index) => (
+          <div className="col-md-3" key={index}>
+            <div className="card mb-4">
+              <img src={product.imageUrl} className="card-img-top" alt="Product Image" />
+              <div className="card-body">
+                <h5 className="card-title">{product.title}</h5>
+                <p className="card-text">${product.price}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Products;
